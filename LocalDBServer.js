39dbc41cbd3d15126e85eb3a85b7bf14e3717868
twmcdunn <twcdunn@gmail.com>
2025-01-16 17:07:22 -0600

    starting to store local db items in ram...

--- LocalDBServer.js
+++ LocalDBServer.js
@@ -29,33 +29,29 @@ var spawn = require('child_process').spawn;
 //console.log("TEST");
 
 class LocalDDBSimulator {
-
+    constructor() {
+        this.dataBase = [];
+        this.dataBase.push(Object.fromEntries([
+            ["EVENT_NUM", -1],
+            ["CURRENT_EVENT_NUM", 0],
+            ["NUM_OF_USERS", 0],
+            ["PIECE_START_TIME", -1]
+        ]));
+    }
 
     query(params, callback) {
         var dataOut;
         var basicBool = precessExpressionAttributeValues(params);
         var Items = [];
-
-        var child = spawn('python3', ['readFakeDB.py', "0"]);
-        child.stdout.on('data', (data) => {
-            //console.log("stdout callback function", data.toString());
-            const lines = data.toString().split("\n");
-            while (!lines[lines.length - 1].includes("EVENT_NUM"))
-                lines.pop();
-            lines.forEach((line) => {
-
-                var refinedBool = basicBool;
-                const cells = line.split(",");
-                //console.log("cells", cells);
-
-                for (let n = 0; n < cells.length - 1; n++) {
-                    refinedBool = refinedBool.replaceAll(cells[n], cells[n + 1]);
-                }
-
-                var value = eval(refinedBool);
-                if (value)
-                    Items.push(createPropertiesObject(cells, params.ProjectionExpression));
+        this.dataBase.forEach((item) => {
+            var refinedBool = basicBool;
+            var map = new Map(Object.entries(item));
+            map.forEach((value, key) => {
+                refinedBool = refinedBool.replaceAll(key, value.N);
             });
+            var value = eval(refinedBool);
+            if (value)
+                Items.push(item);
             dataOut = { Items };
             callback(undefined, dataOut);
         });
@@ -66,64 +62,53 @@ class LocalDDBSimulator {
         var dataOut;
         var Attributes = null;
 
+        var keyMap = new Map(Object.entries(params.Key));
 
-        var child = spawn('python3', ['readFakeDB.py', "0"]);
-        child.stdout.on('data', (data) => {
-            //console.log("stdout callback function", data.toString());
-            const lines = data.toString().split("\n");
-            var dbStr = "";
-            while (!lines[lines.length - 1].includes("EVENT_NUM"))
-                lines.pop();
-            lines.forEach((line) => {
 
-                const cells = line.split(",");
-                var obj = createPropertiesObject(cells);
-                var objMatchesKeys = true;
-                var keyMap = new Map(Object.entries(params.Key));
+        this.dataBase.forEach((item) => {
+            var objMatchesKeys = true;
 
-                keyMap.forEach((value, key) => {
-                    if (obj[key.toString()].N != value.N)
-                        objMatchesKeys = false;
-                });
+            keyMap.forEach((value, key) => {
+                if (item[key.toString()].N != value.N)
+                    objMatchesKeys = false;
+            });
 
-                if (objMatchesKeys) {
-                    Attributes = obj;
-                    obj = structuredClone(obj);
-                    var ue = params.UpdateExpression;
-                    if (ue.startsWith("SET ")) {
-                        ue = ue.replace("SET ", "");
-                        var eavMap = new Map(Object.entries(params.ExpressionAttributeValues));
-                        eavMap.forEach((value, key) => {
-                            ue = ue.replaceAll(key.toString(), value.N.toString());
+            if (objMatchesKeys) {
+                Attributes = item;
+                dataOut = { Attributes };
+                callback(undefined, dataOut);
+
+                if (ue.startsWith("SET ")) {
+                    var itemMap = new Map(item);
+                    ue = ue.replace("SET ", "");
+                    var eavMap = new Map(Object.entries(params.ExpressionAttributeValues));
+                    eavMap.forEach((value, key) => {
+                        ue = ue.replaceAll(key.toString(), value.N.toString());
+                    });
+                    var setStatements = ue.split(",");
+                    itemMap.forEach((value, key) => {
+                        setStatements.forEach((setStatement) => {
+                            if (setStatement.startsWith(cells[n])) {
+                                //perform opperation
+                                var str = "let " + key + " = " + value.N +
+                                    ";\n" + setStatement +
+                                    ";\nitemMap[key] = " + cells[n];
+                                eval(str);
+                                //assignRessult
+                            }
                         });
-                        var setStatements = ue.split(",");
-                        for (let n = 0; n < cells.length - 1; n += 2) {
-                            setStatements.forEach((setStatement) => {
-                                if (setStatement.startsWith(cells[n])) {
-                                    //perform opperation
-                                    var str = "let " + cells[n] + " = " + cells[n + 1] +
-                                        ";\n" + setStatement +
-                                        ";\ncells[n + 1] = " + cells[n];
-                                    eval(str);
-                                    //assignRessult
-                                }
-                            });
-                        }
-                    }
-                    else {
-                        //Console.log("ERROR: the desired update item command \nhasn't been implemented in the local DB simulator\n" + ue);
-                    }
-                    dbStr += cells.join();
+                    })
                 }
                 else {
-                    dbStr += line;// + "\n";
+                    //Console.log("ERROR: the desired update item command \nhasn't been implemented in the local DB simulator\n" + ue);
                 }
-            });
-            var child1 = spawn('python3', ['readFakeDB.py', "1", dbStr]);
-            child1.stdout.on('data', (data) => { });
+            }
+        });
+
+        if(Attributes === null){
             dataOut = { Attributes };
             callback(undefined, dataOut);
-        });
+        }
         return dataOut;
     }
 
@@ -131,71 +116,25 @@ class LocalDDBSimulator {
         var dataOut;
         var Attributes = null;
 
+        this.dataBase.forEach((item) => {
+            var objMatchesKeys = true;
+            var keyMap = new Map(Object.entries(params.Key));
 
-        var child = spawn('python3', ['readFakeDB.py', "0"]);
-        child.stdout.on('data', (data) => {
-            //console.log("stdout callback function", data.toString());
-            const lines = data.toString().split("\n");
-            var dbStr = "";
-            while (!lines[lines.length - 1].includes("EVENT_NUM"))
-                lines.pop();
-            lines.forEach((line) => {
-
-                const cells = line.split(",");
-                var obj = createPropertiesObject(cells);
-                var objMatchesKeys = true;
-                var keyMap = new Map(Object.entries(params.Key));
-
-                keyMap.forEach((value, key) => {
-                    if (obj[key.toString()].N != value.N)
-                        objMatchesKeys = false;
-                });
-
-                if (objMatchesKeys) {
-                    Attributes = obj;
-                }
-                else {
-                    dbStr += line;// + "\n";
-                }
+            keyMap.forEach((value, key) => {
+                if (item[key.toString()].N != value.N)
+                    objMatchesKeys = false;
             });
-            var child1 = spawn('python3', ['readFakeDB.py', "1", dbStr]);
-            child1.stdout.on('data', (data) => { });
-            if (Attributes === null)
-                Attributes = undefined;
-            dataOut = { Attributes };
-            callback(undefined, dataOut);
+            if (objMatchesKeys) {
+                Attributes = item;
+            }
         });
+        dataOut = { Attributes };
+        callback(undefined, dataOut);
         return dataOut;
     }
 
     putItem(params, callback) {
-
-        var map = new Map(Object.entries(params.Item));
-        var cells = []
-        map.forEach((value, key) => {
-            cells.push(key.toString(), value.N.toString());
-        });
-
-        var child = spawn('python3', ['readFakeDB.py', "0"]);
-        child.stdout.on('data', (data) => {
-            //console.log("stdout callback function", data.toString());
-            const lines = data.toString().split("\n");
-            var dbStr = "";
-            while (!lines[lines.length - 1].includes("EVENT_NUM"))
-                lines.pop();
-            lines.forEach((line) => {
-                dbStr += line;// + "\n";
-            });
-            dbStr += cells.join();//  + "\n";
-            var child1 = spawn('python3', ['readFakeDB.py', "1", dbStr]);
-            child1.stdout.on('data', (data) => { });
-        });
-
-        //can't use python's append function because it adds empty lines
-        /*
-        var child1 = spawn('python3', ['readFakeDB.py', "2", "\n" + cells.join()]);
-        child1.stdout.on('data', (data) => { });
-        */
+        this.dataBase.push(Object.entries(params.Item));
 
         callback(undefined, undefined);
 
