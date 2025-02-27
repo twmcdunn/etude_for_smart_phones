2283d81256ed615b3718cb98d1bf77339b98a9cb
twmcdunn <twmcdunn@gmail.com>
2025-02-26 19:55:07 -0600

    integreting gui

--- script.js
+++ script.js
@@ -236,8 +236,10 @@ function checkIfStarted() {
     });
 }
 
+var directionsInterval = -1;
 function startPiece() {
     console.log("STARTING");
+    directionsInterval = setInterval(animateDirections,500);
     var params = {
         ExpressionAttributeValues: {
             ":a": { N: "-1" }
@@ -483,24 +485,111 @@ function writeEventToDB(eventNum, eventVol) {
 
 var instantiateSoundEventButton = null;
 function addInstructionsGraphic() {
+    clearInterval(directionsInterval);
+    directionsInterval = -1;
     console.log("ADDING GRAPHIC @ active sound events =" + activeSoundEvents);
     instantiateSoundEventButton = document.createElement("BUTTON");
     instantiateSoundEventButton.id = "instantiateSoundEventButton";
     instantiateSoundEventButton.innerText = "Instantiate Sound Event";
     instantiateSoundEventButton.onclick = instantiateSoundEvent;
-    document.body.appendChild(instantiateSoundEventButton);
 
+    //document.body.appendChild(instantiateSoundEventButton);
+    var str = "Move your phone DRAMATICALLY!  speed = volume";
+    
+    var dir = document.getElementById("dir");
+    dir.innerText = str;
 }
 
 
 function removeInstructionsGraphic() {
+    directionsInterval = setInterval(animateDirections,500);
+
     console.log("REMOVING GRAPHIC @ active sound events =" + activeSoundEvents);
-    document.body.removeChild(instantiateSoundEventButton);
+    //document.body.removeChild(instantiateSoundEventButton);
 }
 
 
 
-//startPieceLocal();
+
+
+var button = document.createElement("BUTTON");
+button.innerText = "GET PERMISSIONS";
+button.onclick = getPermissions;
+document.body.appendChild(button);
+var dots = 0;
+
+function getPermissions() {
+
+  try {
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+      const permission = DeviceMotionEvent.requestPermission().then((permission) => {
+        //document.body.appendChild(document.createTextNode("PERMISSIONS RESULTS"));
+        if (permission === 'granted') {
+
+        }
+        else {
+
+        }
+        resolvedPermissions();
+      });
+
+      //document.body.appendChild(document.createTextNode("IOS 13 +"));
+      //document.body.appendChild(document.createTextNode("permmission" + permission));
+
+    }
+    else {
+      //document.body.appendChild(document.createTextNode("NOT IOS 13 +"));
+      resolvedPermissions();
+    }
+  }
+  catch (error) {
+    //document.body.appendChild(document.createTextNode("error..."));
+    //document.body.appendChild(document.createTextNode("error: " + error));
+
+  }
+  document.body.removeChild(button);
+
+}
+
+function animateDirections(){
+  var str = "Just listen for now";
+  for(let i = 0; i < dots % 4; i++){
+    str += ".";
+  }
+  dots++;
+  var dir = document.getElementById("dir");
+  dir.innerText = str;
+}
+
+
+var accel = 0;
+
+function resolvedPermissions(){
+  window.ondevicemotion = (event) => {
+    var myAccel = (event.acceleration.x ** 2 + event.acceleration.y ** 2 + event.acceleration.z ** 2) ** 0.5;
+   accel = Math.max(myAccel, accel);
+   if(accel > 20 && myAccel < 1){
+   reportAccel();
+   }
+  };
+}
+
+
+
+function reportAccel(){
+    //document.body.innerText = "accel: " + accel;
+    var dir = document.getElementById("dir");
+    dir.innerText = "accel: " + accel;
+    accel = Math.min(accel, 150);
+    accel -= 20;
+    accel /= 130.0;
+
+    if(directionsInterval === -1){
+      instantiateSoundEvent(accel);
+    }
+
+  accel = 0;
+}
 
 
 
